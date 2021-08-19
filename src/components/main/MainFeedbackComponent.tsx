f92e168cb9ae93a1e63a3084f6f059bec0ed1ce5
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2021-08-19 11:54:33 +0300

    clear messages on success

--- src/components/main/MainFeedbackComponent.tsx
+++ src/components/main/MainFeedbackComponent.tsx
@@ -1,9 +1,9 @@
-import FormAlertComponent, { FormAlert } from "components/core/FormAlertComponent";
-import SubmitButtonComponent, { SubmitButton } from "components/core/SubmitButtonComponent";
-import { useCallback, useMemo, useState } from "react";
-import EmailService from "services/EmailService";
-import { container } from "tsyringe";
-import classes from "./css/MainFeedbackComponent.module.css";
+import FormAlertComponent, { FormAlert } from "components/core/FormAlertComponent"
+import SubmitButtonComponent, { SubmitButton } from "components/core/SubmitButtonComponent"
+import { useCallback, useMemo, useState } from "react"
+import EmailService from "services/EmailService"
+import { container } from "tsyringe"
+import classes from "./css/MainFeedbackComponent.module.css"
 
 function MainFeedbackComponent({ className }: params) {
 	const [email, setEmail] = useState("")
@@ -24,6 +24,8 @@ function MainFeedbackComponent({ className }: params) {
 		e.preventDefault();
 		alert.clearAlert()
 
+		let isSuccessful = false
+
 		try {
 			if (email && name && inquiry) {
 				submitButton.isLoading = true
@@ -33,12 +35,26 @@ function MainFeedbackComponent({ className }: params) {
 					alert.setDangerAlert(error.message)
 				} else {
 					alert.setSuccessAlert("Email has been sent!")
+					isSuccessful = true
 				}
 			}
 		} catch (error) {
 			alert.setDangerAlert(error.message)
 		} finally {
 			submitButton.isLoading = false
+
+			if (isSuccessful) {
+				setEmail("")
+				setName("")
+				setInquiry("")
+
+				await new Promise(resolve =>
+					setTimeout(() => {
+						alert.clearAlert()
+						resolve(undefined)
+					}, 3000)
+				)
+			}
 		}
 	}, [email, name, inquiry, submitButton, alert, emailService])
 
@@ -57,18 +73,18 @@ function MainFeedbackComponent({ className }: params) {
 								<div className="input-group-prepend">
 									<div className="input-group-text">@</div>
 								</div>
-								<input type="email" className="form-control" placeholder="Email" onChange={e => setEmail(e.target.value)} />
+								<input type="email" className="form-control" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
 							</div>
 						</div>
 						<div className="col-12 col-sm-6 form-group">
 							<div className="input-group mb-2">
-								<input type="text" className="form-control" placeholder="Name" onChange={e => setName(e.target.value)} />
+								<input type="text" className="form-control" placeholder="Name" value={name} onChange={e => setName(e.target.value)} />
 							</div>
 						</div>
 					</div>
 					<div className="form-group">
 						<label htmlFor="cs-query-input">Your inquiry:</label>
-						<textarea className="form-control" id="cs-query-input" rows={5} onChange={e => setInquiry(e.target.value)} />
+						<textarea className="form-control" id="cs-query-input" rows={5} value={inquiry} onChange={e => setInquiry(e.target.value)} />
 					</div>
 					<div className="d-flex justify-content-center">
 						<SubmitButtonComponent btn={submitButton} />
--- src/services/EmailService.ts
+++ src/services/EmailService.ts
@@ -1,12 +1,10 @@
-import { singleton } from "tsyringe";
+import { singleton } from "tsyringe"
 
 @singleton()
 export default class EmailService {
-	constructor() { }
-
 	public async sendEmailAsync(): Promise<Error | undefined> {
 		return await new Promise(resolve =>
-			setTimeout(() => resolve(undefined), 3000)
-		);
+			setTimeout(() => resolve(undefined), 1000)
+		)
 	}
 }
